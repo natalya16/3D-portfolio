a748cf221cc45d3d4a4203501c1f16242bafb395
natalya16 <137273166+natalya16@users.noreply.github.com>
2023-08-29 11:50:26 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import {About , Tech , Works , Experience , Feedbacks , Hero, Navbar , StarsCanvas } from './components';
+
+const App = () => {
+  return(
+    <div>
+      3D Developer Portfolio
+    </div>
+  )
+}
+
+export default App
